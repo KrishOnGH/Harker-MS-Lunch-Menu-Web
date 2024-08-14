1641043ea2d5aff3632253df633492b6565ac5f1
Krish, On Annex Account <143832232+krishSNCT@users.noreply.github.com>
2024-08-14 17:02:03 +0530

    reactive menu ui pt. 1
    There is now a base ui available for the user to view the menu. It is reactive and adapts to screen size.

--- web/app/page.js
+++ web/app/page.js
@@ -1,113 +1,256 @@
-import Image from "next/image";
+"use client"
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const scheduleData = [
+  {
+    "day": "Monday",
+    "lunch": {
+      "date": "2024-05-13T00:00:00.000Z",
+      "lunch": [
+        {
+          "food": "Korean BBQ Bowl\nSteamed Rice\nVegetable Stir Fry",
+          "place": "Chef's Grill"
+        },
+        {
+          "food": "(VEG, GF) Spice Trade Beans and Bulgur\n(VEG, GF) Tuscan Portobello Stew",
+          "place": "Veggie Cafe"
+        },
+        {
+          "food": "Deli Bar Served Daily\n(GF) Carving Station: Grilled Chicken with Berry Basil Sauce",
+          "place": "Bistro Garden"
+        },
+        {
+          "food": "Closed for AP Testing",
+          "place": "Mexican Fiesta"
+        },
+        {
+          "food": "Clam Strips with Lemon Dill Coleslaw",
+          "place": "Chowda House"
+        },
+        {
+          "food": "(VEG) Veggie Burger Served Daily\n(GF) Pork Sandwich with Whole Grain Dijonnaise",
+          "place": "Burgerland"
+        },
+        {
+          "food": "(GF) Meat Sauce",
+          "place": "Taste of Italy"
+        }
+      ]
+    }
+  },
+  {
+    "day": "Tuesday",
+    "lunch": {
+      "date": "2024-05-14T00:00:00.000Z",
+      "lunch": [
+        {
+          "food": "(GF) Cilantro Lime Chicken\n(GF) Southwest Corn and Bean Medley\n(GF) Jalapeno Cornbread",
+          "place": "Chef's Grill"
+        },
+        {
+          "food": "(VEG, GF) Tropical Fusion Salad with Spicy Tortilla Strips\n(VEG, GF) Vegetarian Pepperoni Pizza Dip in Bread Bowl",
+          "place": "Veggie Cafe"
+        },
+        {
+          "food": "Deli Bar Served Daily\n(GF) Carving Station: Harissa Spiced Lamb",
+          "place": "Bistro Garden"
+        },
+        {
+          "food": "Closed for AP Testing",
+          "place": "Mexican Fiesta"
+        },
+        {
+          "food": "Haddock Polanaise with Honey Parsley Butter",
+          "place": "Chowda House"
+        },
+        {
+          "food": "(VEG) Veggie Burger Served Daily\n(GF) Warm BBQ Chicken Wrap",
+          "place": "Burgerland"
+        },
+        {
+          "food": "(GF) Sicilian Lentil Pasta Sauce",
+          "place": "Taste of Italy"
+        }
+      ]
+    }
+  },
+  {
+    "day": "Wednesday",
+    "lunch": {
+      "date": "2024-05-15T00:00:00.000Z",
+      "lunch": [
+        {
+          "food": "Sweet and Sour Pork\nVeggie Lo Mein\nSauteed Bok Choy",
+          "place": "Chef's Grill"
+        },
+        {
+          "food": "(VEG, GF) Garlicky Quinoa and Lentils with Ricotta\n(VEG, GF) Potato Samosas",
+          "place": "Veggie Cafe"
+        },
+        {
+          "food": "Deli Bar Served Daily\n(GF) Carving Station: Steak Fajitas with Mango Avocado Salsa",
+          "place": "Bistro Garden"
+        },
+        {
+          "food": "Closed for AP Testing",
+          "place": "Mexican Fiesta"
+        },
+        {
+          "food": "(GF) Mexican Shrimp Cocktails with Saltine Crackers",
+          "place": "Chowda House"
+        },
+        {
+          "food": "(VEG) Veggie Burger Served Daily\n(GF) Turkey Reuben",
+          "place": "Burgerland"
+        },
+        {
+          "food": "(GF) Creamy Pasta Pomodoro",
+          "place": "Taste of Italy"
+        }
+      ]
+    }
+  },
+  {
+    "day": "Thursday",
+    "lunch": {
+      "date": "2024-05-16T00:00:00.000Z",
+      "lunch": [
+        {
+          "food": "(GF) Mojo Marinated Beef\n(GF) Yuca Fries\n(GF) Roasted Asparagus",
+          "place": "Chef's Grill"
+        },
+        {
+          "food": "(VEG, GF) Tofu Rancheros\n(VEG, GF) Bean and Cheese Burritos",
+          "place": "Veggie Cafe"
+        },
+        {
+          "food": "Deli Bar Served Daily\nCarving Station: Vietnamese Caramel Pork with Butterfly Bao Buns",
+          "place": "Bistro Garden"
+        },
+        {
+          "food": "Closed for AP Testing",
+          "place": "Mexican Fiesta"
+        },
+        {
+          "food": "(GF) Smoked Salmon Gyro with Tzatziki and Greek Salsa",
+          "place": "Chowda House"
+        },
+        {
+          "food": "(VEG) Veggie Burger Served Daily\n(GF) Pancake Breakfast Sandwich with Raspberry Maple Syrup",
+          "place": "Burgerland"
+        },
+        {
+          "food": "(GF) Coconut Tomato Sauce",
+          "place": "Taste of Italy"
+        }
+      ]
+    }
+  },
+  {
+    "day": "Friday",
+    "lunch": {
+      "date": "2024-05-17T00:00:00.000Z",
+      "lunch": [
+        {
+          "food": "Oven Baked Fried Chicken\nLoaded Mashed Potatoes\nWilted Mixed Greens",
+          "place": "Chef's Grill"
+        },
+        {
+          "food": "(VEG, GF) Aloo Tamatar Sabzi with Garlic Naan\n(VEG, GF) Crustless Caprese Quiche",
+          "place": "Veggie Cafe"
+        },
+        {
+          "food": "Deli Bar Served Daily\n(GF) Carving Station: Pesto Turkey with Lemon Orzo",
+          "place": "Bistro Garden"
+        },
+        {
+          "food": "Closed for AP Testing",
+          "place": "Mexican Fiesta"
+        },
+        {
+          "food": "(GF) Cod with Cilantro Vinaigrette over Creamed Corn",
+          "place": "Chowda House"
+        },
+        {
+          "food": "(VEG) Veggie Burger Served Daily\n(GF) Chef\u2019s Choice",
+          "place": "Burgerland"
+        },
+        {
+          "food": "(GF) Sweet Corn Pasta with Basil",
+          "place": "Taste of Italy"
+        }
+      ]
+    }
+  }
+]
 
 export default function Home() {
+  const [isSingleView, setIsSingleView] = useState(false);
+  const [maxHeight, setMaxHeight] = useState(0);
+  const entriesRef = useRef([]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsSingleView = window.innerWidth < 830;
+      setIsSingleView(newIsSingleView);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const updateMaxHeight = () => {
+      const heights = entriesRef.current.map(ref => ref?.offsetHeight || 0);
+      const newMaxHeight = Math.max(...heights);
+      setMaxHeight(newMaxHeight);
+    };
+
+    updateMaxHeight();
+
+    const resizeObserver = new ResizeObserver(updateMaxHeight);
+    entriesRef.current.forEach(ref => {
+      if (ref) resizeObserver.observe(ref);
+    });
+
+    return () => resizeObserver.disconnect();
+  }, [isSingleView]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex flex-col items-center space-y-4 p-4 h-screen w-screen overflow-hidden">
+      <div className={`flex ${isSingleView ? 'flex-col' : 'flex-row'} h-full w-full justify-center items-center space-x-[0.5vw]`}>
+        {scheduleData.slice(0, isSingleView ? 1 : 5).map((entry, idx) => (
+          <ScheduleEntry 
+            key={idx} 
+            entry={entry} 
+            maxHeight={maxHeight} 
+            ref={el => entriesRef.current[idx] = el}
+          />
+        ))}
       </div>
     </main>
   );
 }
+
+const ScheduleEntry = React.forwardRef(({ entry, maxHeight }, ref) => {
+  return (
+    <div 
+      ref={ref}
+      className={`bg-white shadow-md rounded-lg flex-none aspect-auto w-[14rem] md:w-[10rem] lg:w-[11rem] xl:w-[15rem] p-4 overflow-y-auto`}
+      style={{ height: maxHeight > 0 ? `${maxHeight}px` : 'auto' }}
+    >
+      <h3 className="font-bold text-lg mb-2 text-gray-800">{entry.day}</h3>
+      <ul>
+        {entry.lunch.lunch.map((item, idx) => (
+          <li key={idx} className="mb-3">
+            <p className="text-xs lg:text-[0.8rem] text-gray-800 font-medium">{item.food}</p>
+            <p className="text-xs text-gray-500">{item.place}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
+
+ScheduleEntry.displayName = 'ScheduleEntry';
